40fa5ea43d72fad90c883ff7c15448ae3437edeb
Gabriel Fagundes <gafagundesmesquita@gmail.com>
2024-06-01 23:13:33 -0300

    Department entity class

---
+++ src/model/entities/Department.java
@@ -0,0 +1,62 @@
+package model.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+// Implementa Serializable para que os arquivos possam ser transformado em sequência de bytes e, portanto, possam trafegar na rede, etc.
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return getId() == that.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+
+}
