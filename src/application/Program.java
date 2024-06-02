cafa928be342270716c1653b4c49298245d5e883
Gabriel Fagundes <gafagundesmesquita@gmail.com>
2024-06-02 00:24:55 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,8 +31,16 @@ public class Program {
         }
 
         System.out.println("\n=== TEST 4: seller insert ===");
-        Seller newSeller = new Seller(null, "Greg", "greg@mail.com", new Date(), 4000.0, department);
+        Seller newSeller = new Seller(null, "Greg", "greg@outlook.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New ID: " + newSeller.getId());
+
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(12);
+        seller.setName("Gregorio");
+        seller.setEmail("greg@gmail.com");
+        sellerDao.update(seller);
+        System.out.println("Updated!");
     }
 }
